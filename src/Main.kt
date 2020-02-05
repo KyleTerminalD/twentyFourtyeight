4fcbfdb2e61a7207293f455fc437c2c2affbe45c
kyleterminald <kyleanderson0112@gmail.com>
2020-02-05 07:40:35 -0500

    Small updates

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,7 @@
 import java.awt.event.KeyEvent
 import kotlin.random.Random
 
+
 fun main(args: Array<String>){
     var puzzle = arrayOf<Array<Int>>()
 
@@ -27,7 +28,7 @@ fun main(args: Array<String>){
     printPuzzle(puzzle)
     for(i in 0..10){
         println("step: $i")
-        printPuzzle(downPuzzle(puzzle))
+        printPuzzle(rightPuzzle(puzzle))
     }
 
 
@@ -141,3 +142,99 @@ fun downPuzzle(puzzle: Array<Array<Int>>) : Array<Array<Int>>{
     return puzzle
 }
 
+fun leftPuzzle(puzzle: Array<Array<Int>>) : Array<Array<Int>>{
+    var move = false
+    for(x in 0..3){
+        for(y in 0..3){
+            if(y < 3 &&  puzzle[x][y] > 0){
+                for(yy in (y+1)..3){
+                    if(puzzle[x][y] == puzzle[x][yy]){
+                        puzzle[x][y] += puzzle[x][yy]
+                        puzzle[x][yy] = 0
+                        move = true
+                        break
+                    } else if(puzzle[x][yy] != 0){
+                        break
+                    }
+                }
+            }
+        }
+    }
+    for(x in 0..3){
+        for(y in 0..3){
+            if(y >= 0 &&  puzzle[x][y] > 0){
+                for(yy in (y-1) downTo 0){
+                    if(puzzle[x][yy] != 0 && yy + 1 != y){
+                        puzzle[x][yy + 1] = puzzle[x][y]
+                        puzzle[x][y] = 0
+                        move = true
+                        break
+                    } else if(puzzle[x][yy] == 0 && yy == 0){
+                        puzzle[x][yy] = puzzle[x][y]
+                        puzzle[x][y] = 0
+                        move = true
+                    }
+                }
+            }
+        }
+    }
+    if(move) {
+        var x: Int
+        var y: Int
+        do {
+            x = Random.nextInt(0, 4)
+            y = Random.nextInt(0, 4)
+        } while (puzzle[x][y] != 0)
+        puzzle[x][y] = Random.nextInt(1, 3) * 2
+    }
+    return puzzle
+}
+
+
+fun rightPuzzle(puzzle: Array<Array<Int>>) : Array<Array<Int>>{
+    var move = false
+    for(x in 0..3){
+        for(y in 3 downTo 0){
+            if(x > 0 &&  puzzle[x][y] > 0){
+                for(xx in (x-1) downTo 0){
+                    if(puzzle[x][y] == puzzle[xx][y]){
+                        puzzle[x][y] += puzzle[xx][y]
+                        puzzle[xx][y] = 0
+                        move = true
+                        break
+                    } else if(puzzle[xx][y] != 0){
+                        break
+                    }
+                }
+            }
+        }
+    }
+    for(x in 0..3){
+        for(y in 3 downTo 0){
+            if(x < 4 &&  puzzle[x][y] > 0){
+                for(xx in (x+1)..3){
+                    if(puzzle[xx][y] != 0 && xx - 1 != x){
+                        puzzle[xx - 1][y] = puzzle[x][y]
+                        puzzle[x][y] = 0
+                        move = true
+                        break
+                    } else if(puzzle[xx][y] == 0 && xx == 3){
+                        puzzle[xx][y] = puzzle[x][y]
+                        puzzle[x][y] = 0
+                        move = true
+                    }
+                }
+            }
+        }
+    }
+    if(move) {
+        var x: Int
+        var y: Int
+        do {
+            x = Random.nextInt(0, 4)
+            y = Random.nextInt(0, 4)
+        } while (puzzle[x][y] != 0)
+        puzzle[x][y] = Random.nextInt(1, 3) * 2
+    }
+    return puzzle
+}
